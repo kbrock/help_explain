ad71205b76a1a1badfe4ccca8f99fb1970ee6dab
Keenan Brock <kbrock@redhat.com>
2013-12-17 21:43:08 -0500

    add sql formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
     icon_name ||= name
     "<i class='icon-#{icon_name}'> #{t(".#{name}", :default => t("helpers.links.#{name}"))}</i>".html_safe
   end
+
+  def format_sql(sql)
+    sql_formatter.format(sql)
+  end
+
+  def sql_formatter
+    @sql_formatter ||= begin
+      rule = AnbtSql::Rule.new
+      rule.keyword = AnbtSql::Rule::KEYWORD_UPPER_CASE
+      %w(count sum substr date).each{|func_name|
+        rule.function_names << func_name.upcase
+      }
+      rule.indent_string = "    "
+      formatter = AnbtSql::Formatter.new(rule)
+    end
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -19,5 +19,13 @@ describe ApplicationHelper do
       expect(helper.icon_and_text('new')).to match /^</
     end
   end
+
+  describe "#format_sql" do
+    it "should upcase keywords" do
+      rslt = helper.format_sql('select * from table')
+      expect(rslt).to match /SELECT/
+      expect(rslt).to match /FROM/
+    end
+  end
 end
 
