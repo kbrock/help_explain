c137659c8da2bfbe8027d7943755c9693a2fedbe
Keenan Brock <kbrock@redhat.com>
2013-12-17 20:36:21 -0500

    big query boxes for sql and query plan

--- spec/views/queries/new.html.erb_spec.rb
+++ spec/views/queries/new.html.erb_spec.rb
@@ -16,9 +16,9 @@ describe "queries/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", queries_path, "post" do
       assert_select "input#query_name[name=?]", "query[name]"
-      assert_select "input#query_comment[name=?]", "query[comment]"
-      assert_select "input#query_sql[name=?]", "query[sql]"
-      assert_select "input#query_plan[name=?]", "query[plan]"
+      assert_select "textarea#query_sql[name=?]", "query[sql]"
+      assert_select "textarea#query_plan[name=?]", "query[plan]"
+      assert_select "textarea#query_comment[name=?]", "query[comment]"
     end
   end
 end
