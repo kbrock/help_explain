cd274ee230e8a68da856ea1c8a5833eed02cfec7
Keenan Brock <kbrock@redhat.com>
2013-12-17 21:25:24 -0500

    clean up sql and plans for basic junk

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,5 +1,34 @@
 class Query < ActiveRecord::Base
+  before_validation :clean_sql
+  before_validation :clean_plan
+
   def plan_summary
     "good stuff"
+  end
+
+  private
+
+  # remove extra whitespace and explain statement
+  def clean_sql
+    if sql.present?
+      self.sql=self.sql
+        .gsub(/^vmdb_development.=/,'')
+        .gsub(/^$\n/,'')
+        .gsub(/\Aexplain.*(select|update|insert|delete)/i) {$1}
+    end
+  end
+
+  def clean_plan
+    if plan.present?
+      self.plan=self.plan
+        .gsub(/\r/,'')
+        .gsub(/ *\+?$/,'')
+        .gsub(/^  */,'')
+        .gsub(/^-*$/,'')
+        .gsub(/^QUERY PLAN$/,'')
+        .gsub(/^vmdb_development.*$/,'')
+        .gsub(/^$\n/,'')
+        .gsub(/^\(.+ rows?\)$/,'')
+    end
   end
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe Query do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#clean_sql" do
+    it "should leave object without explain alone" do
+      expect(clean_sql('select a from table x')).to eq('select a from table x')
+    end
+
+    it "should remove standard explain" do
+      expect(clean_sql('explain analyze verbose select a from table x')).to eq('select a from table x')
+    end
+
+    it "should leave object without explain alone" do
+      expect(clean_sql('explain (analyze on, verbose on, format json) select a from table x')).to eq('select a from table x')
+    end
+  end
+
+  context "#clean_plan" do
+    it "should remove empty lines and spaces" do
+      expect(clean_plan("   +\n  text  +\n  ")).to eq ("text\n")
+    end
+
+    it "should remove QUERY PLAN ---" do
+      expect(clean_plan("\n  QUERY PLAN  \n---------\n  text  +\n  ")).to eq ("text\n")
+    end
+
+    it "should remove sql prompts" do
+      expect(clean_plan("
+        vmdb_development=# select something great
+        vmdb_development(= select the rest;
+          text  +\n  ")).to eq ("text\n")
+    end
+
+    it "should remove rows entry at the end" do
+      expect(clean_plan("
+          text  +\n (3 rows)  ")).to eq ("text\n")
+    end
+  end
+
+  protected
+  def clean_sql(str)
+    Query.new(sql: str).tap{|q| q.send(:clean_sql) }.sql
+  end
+
+  def clean_plan(str)
+    Query.new(plan: str).tap{|q| q.send(:clean_plan) }.plan
+  end
 end
